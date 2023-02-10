cad533d47f75e4597d6324d941cbf7dec3973f62
KAVIARASU C <77060075+KAVIARASU0505@users.noreply.github.com>
2023-02-10 13:40:30 +0530

    Update Day11.js

--- CSS/Day11/Day11.js
+++ CSS/Day11/Day11.js
@@ -6,8 +6,6 @@ document.getElementById('go-forward').addEventListener('click', function(){
    history.forward();
    console.log("clicked");
 })
-/*const url = 'sample.html';
-history.pushState('null','null', url);*/
 
 const options = {
    enableHighAccuracy: true,
@@ -20,24 +18,3 @@ const options = {
  }
  navigator.geolocation.getCurrentPosition(success, error, options);
 
-/*let id;
-let target;
-let options;
-function success(pos) {
-  const crd = pos.coords;
-  if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
-    console.log('Congratulations, you reached the target');
-    navigator.geolocation.clearWatch(id);
-  }
-}
-function error(err) {
-  console.error('ERROR');
-}
-target = {
-  latitude : 0,
-  longitude: 0
-};
-options = {
-  enableHighAccuracy: false,
-};
-id = navigator.geolocation.watchPosition(success, error, options);*/
