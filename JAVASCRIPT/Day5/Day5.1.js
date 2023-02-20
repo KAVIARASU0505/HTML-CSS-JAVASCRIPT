8265a75676483b7b76d83da7fe193453d1f39577
KAVIARASU C <77060075+KAVIARASU0505@users.noreply.github.com>
2023-02-20 11:30:11 +0530

    Update Day5.1.js

--- JAVASCRIPT/Day5/Day5.1.js
+++ JAVASCRIPT/Day5/Day5.1.js
@@ -1,8 +1,11 @@
 ////This file is used for Import and export
 ////Imported the file from Day5.js and implemented in Day5.1.js
-import  {paraself,context} from  './Day5.js';
-paraself(["Naveen",25]);
-context({name:"Savi",age:23});
+// import  {paraself,context} from  './Day5.js';
+// paraself(["Naveen",25]);
+// context({name:"Savi",age:23});
+import Rectangle from './Day5.js'; // not {User}, just User
+var cont=new Rectangle(10,12);
+cont.test();
 
 // import rule from './Day5.js';
 // good();
